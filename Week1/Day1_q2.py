838759a1b34b4eb3d25c7b09a002f733c20bd5f4
anmolubhi1 <anmolubhi76@gmail.com>
2025-06-30 12:05:46 +0530

    Delete Week1/Day1_q2.py

--- Week1/Day1_q2.py
+++
@@ -1,5 +0,0 @@
-principal=float(input("Principal amount: "))
-rate=float(input("Interest: "))
-time=float(input("Time: "))
-si=(principal * rate * time)/100
-print("Value of simple interest is:",si)
